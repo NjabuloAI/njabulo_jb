102abcf8a519f91076a12f68f4d778828c82763b
NjabuloJb <fanajb65@gmail.com>
2025-09-11 20:00:16 +0200

    Update img.js

--- commandes/img.js
+++ commandes/img.js
@@ -1,33 +1,41 @@
-
-
-
-const {zokou} = require('../framework/zokou');
+const { zokou } = require('../framework/zokou');
 var gis = require('g-i-s');
-
 
 zokou({
   nomCom: "img",
   categorie: "Search",
   reaction: "📷"
-},
-async (dest, zk, commandeOptions) => {
+}, async (dest, zk, commandeOptions) => {
   const { repondre, ms, arg } = commandeOptions;
 
   if (!arg[0]) {
-    repondre('which image ? !');
+    repondre('Which image? !');
     return;
   }
 
   const searchTerm = arg.join(" ");
-  //repondre("termes " +searchTerm);
-  gis(searchTerm,envoiImage);
+  gis(searchTerm, envoiImage);
 
-  function envoiImage(e,r)
-   {
-        if(e){repondre("oups une error ")}else{for(var a=0;a<5;a++){zk.sendMessage(dest,{image:{url:r[a].url}},{quoted:ms});}}
-    
-   }
+  function envoiImage(e, r) {
+    if (e) {
+      repondre("Oops, an error occurred!");
+    } else {
+      let buttons = [];
+      for (var a = 0; a < 5; a++) {
+        buttons.push({
+          buttonId: `viewimg_${a}`,
+          buttonText: `View Image ${a + 1}`,
+          type: 1
+        });
+      }
 
- //gis(searchTerm,envoiImage);
-      
+      zk.sendMessage(dest, {
+        image: { url: r[0].url },
+        caption: `Search results for ${searchTerm}`,
+        footer: 'Select an image to view',
+        buttons: buttons,
+        headerType: 4
+      }, { quoted: ms });
+    }
+  }
 });
