629ae3aa5cdb42682ebb64ac20a3c129bc052fea
NjabuloJb <fanajb65@gmail.com>
2025-09-13 23:02:39 +0200

    Update xmdimg.js

--- commandes/xmdimg.js
+++ commandes/xmdimg.js
@@ -11,8 +11,44 @@ fana({
 }, async (dest, zk, commandeOptions) => {
   const { repondre, ms, arg } = commandeOptions;
 
+      // List of image URLs
+    const njabulox = [
+        "https://files.catbox.moe/iii5jv.jpg",
+        "https://files.catbox.moe/xjeyjh.jpg",
+        "https://files.catbox.moe/mh36c7.jpg",
+        "https://files.catbox.moe/u6v5ir.jpg",
+        "https://files.catbox.moe/bnb3vx.jpg" // New image added
+    ];
+
+    // Select a random image file
+    const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
+    
+
   if (!arg[0]) {
-    repondre('Which image?');
+    zk.sendMessage(dest, {
+    text: "Wich image name you want",
+    contextInfo: {
+     externalAdReply: {
+         title: "💓ᥕᥱᥣᥴomᥱ fᥲmιᥣყ ",
+         mediaType: 1,
+          previewType: 0,
+         thumbnailUrl: randomNjabulourl,
+         renderLargerThumbnail: false,
+        },
+        },
+          }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "njᥲbᥙᥣo",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
     return;
   }
 
@@ -33,6 +69,7 @@ fana({
     for (let i = 0; i < Math.min(results.length, 5); i++) {
       zk.sendMessage(dest, {
         image: { url: results[i].url },
+        caption: `Njabulo Jb downloader image`,
         contextInfo: {
          isForwarded: true,
          forwardedNewsletterMessageInfo: {
