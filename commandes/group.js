7cb2de6b68ccb2d9385d5f3b336a759b069b15be
NjabuloJb <fanajb65@gmail.com>
2025-09-17 20:44:42 +0200

    Create group.js

---
+++ commandes/group.js
@@ -0,0 +1,61 @@
+const { fana } = require("../njabulo/fana")
+//const { getGroupe } = require("../bdd/groupe")
+const { Sticker, StickerTypes } = require('wa-sticker-formatter');
+const {ajouterOuMettreAJourJid,mettreAJourAction,verifierEtatJid} = require("../bdd/antilien")
+const {atbajouterOuMettreAJourJid,atbverifierEtatJid} = require("../bdd/antibot")
+const { search, download } = require("aptoide-scraper");
+const fs = require("fs-extra");
+const conf = require("../set");
+const { default: axios } = require('axios');
+
+    // List of image URLs
+    const njabulox = [
+        "https://files.catbox.moe/iii5jv.jpg",
+        "https://files.catbox.moe/xjeyjh.jpg",
+        "https://files.catbox.moe/mh36c7.jpg",
+        "https://files.catbox.moe/u6v5ir.jpg",
+        "https://files.catbox.moe/bnb3vx.jpg" // New image added
+    ];
+
+    // Select a random image file
+    const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
+    
+
+async function sendFormattedMessage(zk, chatId, text, ms) {
+  await zk.sendMessage(chatId, {
+    text,
+    contextInfo: {
+     externalAdReply: {
+         title: "⏰ message group open & close",
+         mediaType: 1,
+          previewType: 0,
+         thumbnailUrl: randomNjabulourl,
+         renderLargerThumbnail: false,
+        },
+        },
+          }, { quoted: ms });
+}
+
+fana({ nomCom: "group", categorie: 'Group' }, async (dest, zk, commandeOptions) => {
+  const { repondre, verifGroupe, verifAdmin, superUser, arg, ms } = commandeOptions;
+
+  if (!verifGroupe) return await sendFormattedMessage(zk, dest, "σrdєr rєsєrvєd fσr grσup σnlч", ms);
+
+  if (!(superUser || verifAdmin)) return await sendFormattedMessage(zk, dest, "σrdєr rєsєrvєd fσr thє αdmínístrαtσrr", ms);
+
+  if (!arg[0]) return await sendFormattedMessage(zk, dest, 'Instructions:\n\nType group open or close', ms);
+
+  const option = arg.join(' ').toLowerCase();
+  switch (option) {
+    case "open":
+      await zk.groupSettingUpdate(dest, 'not_announcement');
+      await sendFormattedMessage(zk, dest, 'group open', ms);
+      break;
+    case "close":
+      await zk.groupSettingUpdate(dest, 'announcement');
+      await sendFormattedMessage(zk, dest, 'grσup lσck succєssfullч 🔒', ms);
+      break;
+    default:
+      await sendFormattedMessage(zk, dest, "plєαsє dσn't ínvєnt αn σptíσn", ms);
+  }
+});
