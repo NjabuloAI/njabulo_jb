b20c42d31d4be8b8180862108b5341c1da9c08d7
NjabuloJb <fanajb65@gmail.com>
2025-09-19 14:27:46 +0200

    Create lyrics.js

---
+++ commandes/lyrics.js
@@ -0,0 +1,37 @@
+const { zokou } = require("../framework/zokou");
+const axios = require('axios');
+
+zokou({
+  nomCom: "lyrics",
+ 3,
+  aliases: ["lyric"],
+  categorie: "search",
+  reaction: "💫"
+}, async (dest, zk, commandOptions) => {
+  const { arg, ms, userJid } = commandOptions;
+
+  try {
+    if (!arg) {
+      return zk.sendMessage(dest, { text: 'Please provide a song name or artist.' });
+    }
+
+    const query = arg.join(' ');
+    const apiURL = `https://api.lyrics.ovh/v1/${query}`;
+
+    const response = await axios.get(apiURL);
+    const data = response.data;
+
+    if (!data.lyrics) {
+      return zk.sendMessage(dest, { text: 'No lyrics found for your query.' });
+    }
+
+    const lyricsMessage = {
+      text: `*${query}*\n\n${data.lyrics}`,
+    };
+
+    await zk.sendMessage(dest, lyricsMessage);
+  } catch (err) {
+    console.error('[LYRICS] Error:', err);
+    await zk.sendMessage(dest, { text: 'An error occurred: ' + err.message });
+  }
+});
